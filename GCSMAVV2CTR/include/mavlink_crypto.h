6495e3e56f662152f723344fe951a0cf5e1a6856
MarkSei <s114740@student.dtu.dk>
2018-01-25 13:27:43 +0100

    add ctr mode in v2

---
+++ GCSMAVV2CTR/include/mavlink_crypto.h
@@ -0,0 +1,70 @@
+#pragma once
+
+/**
+  * MAVLink v1.0 Crypto-headers
+  * @Author: Mark Seidenschnur
+  * Description: Headers adding the Chaskey signature scheme
+  * and AES encryption.
+  */
+
+#ifndef  _MAVLINK_CRYPTO_H_
+#define  _MAVLINK_CRYPTO_H_
+
+#include "crypto/chaskey.c"
+#include "crypto/aes.c"
+
+#ifndef MAVLINK_CRYPTO
+#define MAVLINK_CRYPTO
+#endif
+
+MAVLINK_CRYPTO void chaskey_sign(const uint8_t* msg, const uint8_t len, uint8_t* tag, uint32_t tlen, uint8_t* key)
+{
+    uint32_t k[4], k1[4], k2[4];
+
+    uint32_t u;
+    for(unsigned int i = 0; i < 4; i++)
+    {
+    	u = key[0];
+	    u = (u << 8) + key[4*i + 0];
+	    u = (u << 8) + key[4*i + 1];
+	    u = (u << 8) + key[4*i + 2];
+
+	    k[i] = u;
+    }
+
+	uint32_t taglen = tlen;
+	uint32_t msglen = len;
+	
+	subkeys(k1,k2,k);
+	chaskey(tag, taglen, msg, msglen, k, k1, k2);
+
+}
+
+MAVLINK_CRYPTO void aes_encrypt(uint8_t* cipher, uint8_t len, uint8_t* key, uint8_t* nonce)
+{
+	struct AES_ctx ctx;
+	uint32_t msglen = (uint32_t)len;
+
+	uint8_t iv[16];
+	memset(iv, 0x00, 16);
+	memcpy(iv, nonce, 6);
+
+	AES_init_ctx_iv(&ctx, key, iv);
+	AES_CTR_xcrypt_buffer(&ctx, cipher, msglen);
+}
+
+MAVLINK_CRYPTO void aes_decrypt(uint8_t* msg, uint8_t len, uint8_t* key, uint8_t* nonce)
+{
+	struct AES_ctx ctx;
+	uint32_t msglen = (uint32_t)len;
+
+	uint8_t iv[16];
+	memset(iv, 0x00, 16);
+	memcpy(iv, nonce, 6);
+
+	AES_init_ctx_iv(&ctx, key, iv);
+	AES_CTR_xcrypt_buffer(&ctx, msg, msglen);
+}
+
+
+#endif /* _MAVLINK_HELPERS_H_ */
---
+++ GCSMAVV2CTR/src/main.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <assert.h>
+
+#include <common/mavlink.h>
+
+#define BUFSIZE 2041 //1024
+
+int print_buffer(uint8_t* buf, uint16_t len) {
+
+    if (len > MAVLINK_MAX_PACKET_LEN || len < MAVLINK_NUM_NON_PAYLOAD_BYTES + 1) return 0;
+
+    for (unsigned int i = 0; i < len; ++i) printf("%02x", buf[i]);
+
+    return 1;
+}
+
+int main(void) {
+
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+
+	uint64_t curr = (tv.tv_sec - 1420070400) * 100000 + tv.tv_usec / 10;
+
+	mavlink_signing_t signing;
+	mavlink_signing_streams_t signing_streams;
+
+	uint8_t key[] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c,
+					  0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+
+
+	memcpy(signing.secret_key, key, 32);
+	signing.link_id = (uint8_t) MAVLINK_COMM_0;
+	signing.timestamp = curr;
+	signing.flags = MAVLINK_SIGNING_FLAG_SIGN_OUTGOING;
+	
+	mavlink_init_secure_chan(&signing, MAVLINK_ROLE_GCS);
+
+
+	mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+	status->signing = &signing;
+
+	
+	status->signing_streams = &signing_streams;
+
+	
+	mavlink_message_t msg;
+	mavlink_msg_heartbeat_pack(1, 200, &msg, MAV_TYPE_GCS, MAV_AUTOPILOT_INVALID, 0, 0, 0);
+
+	uint8_t buf[BUFSIZE];
+	uint16_t len;
+	len = mavlink_msg_to_send_buffer(buf, &msg);
+
+	assert(print_buffer(buf,len));
+	printf("\n");
+	
+	mavlink_init_secure_chan(&signing, MAVLINK_ROLE_UAV);
+	mavlink_message_t msg_rec;
+    mavlink_heartbeat_t heartbeat;
+
+	for (unsigned int i = 0; i < len; ++i)
+    {
+      //printf("%02x ", (unsigned char)temp);
+      if (mavlink_parse_char(MAVLINK_COMM_0, buf[i], &msg_rec, status))
+      {
+        // Packet received
+        //mavlink_msg_heartbeat_decode(&msg, &hb);
+        if(msg_rec.msgid == 0)
+        {
+			len = mavlink_msg_to_send_buffer(buf, &msg_rec);
+			assert(print_buffer(buf,len));
+        }
+        mavlink_msg_heartbeat_decode(&msg_rec, &heartbeat);
+        
+      } 
+    }
+
+    printf("\n");
+
+	
+	return 0;
+}
